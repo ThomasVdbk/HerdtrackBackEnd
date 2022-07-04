506ef7ead14c1d947b6422b8a13b38f2def8c068
Alexandre Dessoly <alexandre.dessoly1@gmail.com>
2022-07-04 14:02:35 +0200

    first commit

---
+++ src/main/java/com/herdtrack/herdtrackbackend/HerdtrackBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.herdtrack.herdtrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerdtrackBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerdtrackBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herdtrack/herdtrackbackend/HerdtrackBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herdtrack.herdtrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerdtrackBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
