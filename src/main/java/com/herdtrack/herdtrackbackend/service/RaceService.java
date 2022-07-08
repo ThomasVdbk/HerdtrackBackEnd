2fb3fc77eae9085ba4c24068835df3df62570a3f
Thomas Screve <thomas-scrve@laposte.net>
2022-07-08 14:18:23 +0200

    Services Race, Contact, Vaccin créés

--- src/main/java/com/herdtrack/herdtrackbackend/service/RaceService.java
+++ src/main/java/com/herdtrack/herdtrackbackend/service/RaceService.java
@@ -1,12 +1,63 @@
 package com.herdtrack.herdtrackbackend.service;
 
+import com.herdtrack.herdtrackbackend.entity.Race;
+import com.herdtrack.herdtrackbackend.entity.Vaccin;
 import com.herdtrack.herdtrackbackend.repository.RaceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RaceService {
 
     @Autowired
     private RaceRepo raceRepo;
+
+    public List<Race> findAll(){
+        return this.raceRepo.findAll();
+    }
+    public Race findById(Long id) {
+        Optional<Race> optRace = this.raceRepo.findById(id);
+        if (optRace.isPresent()) {
+            return optRace.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+public List<Race> findByLibelle(String libelle) {
+    return this.raceRepo.findByLibelle(libelle);
+}
+    public Race create(Race newRace) {
+        return this.raceRepo.save(newRace);
+    }
+
+    public Race update(Race race) {
+        if (! this.raceRepo.existsById(race.getId())) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE,
+                    "impossible de trouver le contact a mettre à jour");
+        }
+        return this.raceRepo.save(race);
+    }
+
+    public Race delete (Long id) {
+        if (! this.raceRepo.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "impossible de trouver le contact à supprimer");
+        }
+        Race raceDel = this.findById(id);
+        this.raceRepo.deleteById(id);
+        if (this.raceRepo.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.EXPECTATION_FAILED,
+                    "erreur lors de la suppression du contact");
+        }
+
+        return raceDel;
+    }
+
+
+
 }
--- src/main/java/com/herdtrack/herdtrackbackend/service/VaccinService.java
+++ src/main/java/com/herdtrack/herdtrackbackend/service/VaccinService.java
@@ -1,13 +1,74 @@
 package com.herdtrack.herdtrackbackend.service;
 
+import com.herdtrack.herdtrackbackend.entity.Vaccin;
 import com.herdtrack.herdtrackbackend.repository.VaccinRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VaccinService {
 
     @Autowired
     private VaccinRepo vaccinRepo;
+
+
+    public List<Vaccin> findAll(){
+        return this.vaccinRepo.findAll();
+    }
+    public Vaccin findById(Long id) {
+        Optional<Vaccin> optVaccin = this.vaccinRepo.findById(id);
+        if (optVaccin.isPresent()) {
+            return optVaccin.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+    public List<Vaccin> findByLibelle(String libelle) {
+        return this.vaccinRepo.findByLibelle(libelle);
+    }
+
+    public List<Vaccin> findByNumero (String numero_lot){
+        return this.vaccinRepo.findByNumero(numero_lot);
+    }
+    public List<Vaccin> findByDate (Date date){
+        return this.vaccinRepo.findByDate(date);
+    }
+    public List<Vaccin> findByDays (int nbrJoursProchain){
+        return this.vaccinRepo.findByDays(nbrJoursProchain);
+    }
+    public Vaccin create(Vaccin newVaccin) {
+        return this.vaccinRepo.save(newVaccin);
+    }
+
+    public Vaccin update(Vaccin vaccin) {
+        if (! this.vaccinRepo.existsById(vaccin.getId())) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE,
+                    "impossible de trouver le contact a mettre à jour");
+        }
+        return this.vaccinRepo.save(vaccin);
+    }
+
+    public Vaccin delete (Long id) {
+        if (! this.vaccinRepo.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "impossible de trouver le contact à supprimer");
+        }
+        Vaccin vaccinDel = this.findById(id);
+        this.vaccinRepo.deleteById(id);
+        if (this.vaccinRepo.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.EXPECTATION_FAILED,
+                    "erreur lors de la suppression du contact");
+        }
+
+        return vaccinDel;
+    }
+
+
 
 }
