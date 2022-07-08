d0414a05c26ab4f6fd447fb32dd23e4136f6a993
Thomas Screve <thomas-scrve@laposte.net>
2022-07-08 10:08:01 +0200

    entity Race fait

--- src/main/java/com/herdtrack/herdtrackbackend/entity/Contact.java
+++ src/main/java/com/herdtrack/herdtrackbackend/entity/Contact.java
@@ -9,39 +9,48 @@ import javax.validation.constraints.NotNull;
 public class Contact {
 
 
+    // This is a JPA annotation that tells the database to auto-generate the id for each new record.
     @Id
     @GeneratedValue
     private  Long id;
 
 
+    // This is a JPA annotation that tells the database to create a column called last_name with a length of 45 characters.
     @Column(name = "last_name", length = 45)
     @NotNull
     private String lastName;
 
 
 
+    // This is a JPA annotation that tells the database to create a column called first_name with a length of 45
+    // characters.
     @Column(name = "first_name", length = 45)
     @NotNull
     private String firstName;
 
 
+  // This is a JPA annotation that tells the database to create a column called relation with a length of 45 characters.
     @Column(name = "relation", length = 45)
     private String relation;
 
 
+    // This is a JPA annotation that tells the database to create a column called phone with a length of 45 characters.
     @Column(name = "phone",length = 45)
     @NotNull
     private String phone;
 
+    // This is a default constructor. It is required by JPA.
     public Contact() {
     }
 
+    // This is a constructor that takes in the parameters and sets them to the class variables.
     public Contact(String lastName, String firstName, String relation, String phone) {
         this.lastName = lastName;
         this.firstName = firstName;
         this.relation = relation;
         this.phone = phone;
     }
+
 
     public Contact(Long id, String lastName, String firstName, String relation, String phone) {
         this.id = id;
--- src/main/java/com/herdtrack/herdtrackbackend/entity/Race.java
+++ src/main/java/com/herdtrack/herdtrackbackend/entity/Race.java
@@ -1,4 +1,52 @@
 package com.herdtrack.herdtrackbackend.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "race")
 public class Race {
+
+    // Creating a primary key for the table.
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+    // Creating a column in the database called libelle with a length of 45 characters and it is not null.
+    @Column(name = "libelle",length = 45)
+    @NotNull
+    private String libelle;
+
+
+    // A void constructor.
+    public Race() {
+    }
+
+    // A constructor.
+    public Race(String libelle) {
+        this.libelle = libelle;
+    }
+
+
+    public Race(Long id, String libelle) {
+        this.id = id;
+        this.libelle = libelle;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
 }
