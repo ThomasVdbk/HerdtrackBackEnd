d15dc49ab111b3eb8c62d26aaa1095b5a27ebe67
Alexandre Dessoly <alexandre.dessoly1@gmail.com>
2022-07-10 17:28:27 +0200

    feat medicament entity

--- src/main/java/com/herdtrack/herdtrackbackend/Entity/Medicament.java
+++ src/main/java/com/herdtrack/herdtrackbackend/Entity/Medicament.java
@@ -1,10 +1,74 @@
 package com.herdtrack.herdtrackbackend.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+/**
+ * Classe medicament
+ * @author Alexandre Dessoly
+ */
 @Entity
 @Table(name="medicament")
 public class Medicament {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="numerolot",nullable = false)
+    private String numeroLot;
+
+    @Column(name="libelle",nullable = false)
+    private String libelle;
+
+    /**
+     * Constructeur
+     * @Author Alexandre Dessoly
+     */
+
+    public Medicament(){
+
+    }
+    public Medicament(Long id) {
+        this.id = id;
+    }
+
+    public Medicament(Long id, String numeroLot) {
+        this.id = id;
+        this.numeroLot = numeroLot;
+    }
+
+    public Medicament(Long id, String numeroLot, String libelle) {
+        this.id = id;
+        this.numeroLot = numeroLot;
+        this.libelle = libelle;
+    }
+
+    /**
+     * Getters && Setters
+     * @Author Alexandre Dessoly
+     */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroLot() {
+        return numeroLot;
+    }
+
+    public void setNumeroLot(String numeroLot) {
+        this.numeroLot = numeroLot;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
 }
